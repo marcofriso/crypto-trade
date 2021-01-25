edf4794c3965bffffff13cfab5954e365cc0b773
Marco F <marco.friso@gmail.com>
2021-01-25 13:23:18 +0100

    feat: implements routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AppStyle from "./AppStyle";
+import Currency from "./Components/Currency";
+import Home from "./Components/Home";
 
 const App = () => (
-  <AppStyle>
-    <h1>Crypto Trade</h1>
-  </AppStyle>
+  <BrowserRouter>
+    <AppStyle>
+      <h1>Crypto Trade</h1>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/crypto/:id" component={Currency} exact />
+      </Switch>
+    </AppStyle>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/Components/Currency.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Currency = () => (
+  <div>
+    <h2>Currency</h2>
+  </div>
+);
+
+export default Currency;
