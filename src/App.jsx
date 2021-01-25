c82e861d67f5ab383e6ea53b01f160770c3c48b8
Marco F <marco.friso@gmail.com>
2021-01-25 13:06:44 +0100

    chore: add `styled-components`

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppStyle from "./AppStyle";
+
+const App = () => (
+  <AppStyle>
+    <h1>Crypto Trade</h1>
+  </AppStyle>
+);
+
+export default App;
---
+++ src/AppStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const AppStyle = styled.div`
+  text-align: center;
+  max-width: 1600px;
+  margin: auto;
+`;
+
+export default AppStyle;
